4477ab988100cd08e73aabd04b01656e3b45db69
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-30 18:42:56 -0800

    imported react/bs components

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./About.css";
 import Headshot from "../../images/Headshot.jpg";
+import { Row, Col } from 'react-bootstrap';
 
 export default function About() {
   return (
@@ -8,26 +9,31 @@ export default function About() {
       <div id="app-modal"/>
       <div className="background">
         <h1 className="about-me-header">About Me</h1>
-        <div className="about-me-row">
+        <Row className="about-me-row">
           
-          <div className="col-4">
+          <Col className="col-4">
             <img
               src={Headshot}
               alt="Gabrielle Humkey headshot"
               className="img-headshot"
             />
-          </div>
+          </Col>
          
-          <div className="col-8">
+          <Col className="col-8">
             <p className="about-me-text">
-            I am a full stack web developer with a passion for creating polished front end designs and accessible user interfaces. 
-            I possess a strong commitment to developing customer friendly and easy to use applications. 
-            I am a proactive, agile practicing leader with strong communication and collaboration skills. 
-            I am eager to learn and further refine my knowledge in coding languages and structures.
+            Full stack web developer with a passion for creating polished front end designs and accessible user interfaces. 
+            Strong commitment to developing customer friendly and easy to use applications. 
+            Proactive, agile practicing leader with strong communication and collaboration skills.
+            <br/> 
+            I have had the unique experience of completing a psychology degree and 
+            developing fluid social skills through leading staff members and working with children, 
+            but now wish to pivot in my career trajectory. 
+            As a result, I opted to enroll and successfully complete the UW Coding Bootcamp and 
+            wish to continue refining my knowledge in the coding world. 
             </p>
-          </div>
+          </Col>
 
-        </div>
+        </Row>
 
         <a
           className="btn btn-resume btn-warning"
